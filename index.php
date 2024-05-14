ddf41b2736a5dc6d23486d0278538339e03661c4
lucas dechavanne <lucasdechavanne22@gmail.com>
2024-05-14 09:59:09 +0200

    push final

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
             ?>
                 <section class="destination text-white d-flex align-items-center justify-content-around col-12" style="background-image: url(./images/images/destination/<?=$location?>.jpg);">
                     <div class="col-3 d-flex justify-content-center align-items-center" style="background-color: rgba(0, 0, 0, 0.5);">
-                        <p class="fs-1"><?=$location?></p>
+                        <p class="fs-1"><?=$location?>poirz<?php ozrigzie eao oz gorz ozr lz etu?></p>
                     </div>   
                     <div class="col-5"> 
                         <a href="./pages/details.php?Destination=<?=$location?>"><button class="btn btn-outline-light m-3">Voir les offres<i class="fa-solid fa-angles-right ms-3" style="color: #ffffff;"></i></button></a> 
